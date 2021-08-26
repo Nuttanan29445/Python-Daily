73a77fdf1d0b470c116eba948915364b46470c8b
Nuttanan29445 <nuch29445@gmail.com>
2021-08-26 22:38:10 +0700

    capitalLetter

---
+++ Day018.py
@@ -0,0 +1,7 @@
+x = input("Input: ")
+y= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+n = 0
+for i in x:
+    if(i in y):
+        n = n+1
+print("Num of capital letter:",n)
